f19d83ff5f508225d33d48637148ffed830ff36d
VladTestPage <kissntrill@gmail.com>
2022-11-01 18:15:25 +0300

    add: fourth task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+// подключение модулей
+const fs = require('fs')
+const path = require('path')
 
+// назначенией путей переменным
+const directorySource = path.join(__dirname, 'files')
+const directoryCopy = path.join(__dirname, 'files-copy')
+
+// создание папки
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, () => {})
+
+// очистка папки
+fs.readdir(directoryCopy, (err, files) => {
+  if (err) throw err
+
+  for (const file of files) {
+    fs.unlink(path.join(directoryCopy, file), (err) => {
+      if (err) throw err
+    })
+  }
+})
+
+// чтение содержания исходной папки и копирование файлов в новую папку
+fs.readdir(directorySource, (err, files) => {
+  for (const file of files) {
+    fs.copyFile(
+      path.join(directorySource, file),
+      path.join(directoryCopy, file),
+      () => {}
+    )
+  }
+})
